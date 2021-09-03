5a28de30ae927f0db6151e6a8629265b31a935b1
magno <magno@MAGNO-NOTEBOOK>
2021-09-03 01:45:50 -0300

    Adicionar arquivos de projeto.

---
+++ Teste_Git_Notebook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Teste_Git_Notebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+
+
+
+            
+        }
+    }
+}
